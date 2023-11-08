97e0502ae0616ccad41ad911c7c6a21706f82694
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-08 16:43:29 +0100

    feat: created footer component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,25 @@
-"use client"
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { useQuery } from "@tanstack/react-query"
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
-  const [hello, setHello] = useState<string>("")
+  const [hello, setHello] = useState<string>("");
   const fetchHello = async () => {
     let response = await axios.get("http://localhost:4000/hello");
-    setHello(response.data)
-  }
- useEffect(()=> {fetchHello();}, [])
+    setHello(response.data);
+  };
+  useEffect(() => {
+    fetchHello();
+  }, []);
 
-
-  
- return (
-    <main>
-      <h1>{hello}</h1>
-    </main>
-  )
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>{hello}</h1>
+      </main>
+      <Footer />
+    </>
+  );
 }
---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="h-15 bottom-0 sticky flex flex-col items-center bg-orange-50 text-center text-orange-950 border-t-orange-950">
+      <div className="w-full flex justify-between items-center px-8 py-4 text-center">
+        <Link href="/about">About</Link>
+        <a href="https://github.com/Marisa-Pinheiro">GitHub</a>
+      </div>
+    </footer>
+  );
+}
