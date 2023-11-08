dc1ad0558a59dea67acdcf3476466d9c61b736a1
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-08 11:50:37 +0100

    feat: fetching information from backend endpoint

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,18 @@
+"use client"
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Home() {
   const [hello, setHello] = useState<string>("")
-  const fetchHello = () => {
-    let data = axios.get();
-
-
+  const fetchHello = async () => {
+    let response = await axios.get("http://localhost:4000/hello");
+    setHello(response.data)
   }
- 
-  return (
+ useEffect(()=> {fetchHello();}, [])
+  
+ return (
     <main>
-
+      <h1>{hello}</h1>
     </main>
   )
 }
