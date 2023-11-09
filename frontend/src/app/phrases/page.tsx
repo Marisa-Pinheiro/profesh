04b4c853bcc2cf943067f6fd4ff71c77f44c843c
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-09 10:17:36 +0100

    feat: displaying informal sentences on phrases page

--- frontend/src/app/phrases/page.tsx
+++ frontend/src/app/phrases/page.tsx
@@ -4,27 +4,50 @@ import axios from "axios";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
+type Phrase = {
+  id: string;
+  informalPhrase: string;
+  formalPhrase: string;
+};
+
 const Page = () => {
-  const [hello, setHello] = useState<string>("");
-  const fetchHello = async () => {
-    let response = await axios.get("http://localhost:4000/hello");
-    setHello(response.data);
+  const [phrases, setPhrases] = useState<Phrase[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState();
+
+  const fetchPhrases = async () => {
+    const response = await axios.get("http://localhost:4000/phrases");
+    setPhrases(response.data);
+    setLoading(false);
   };
+
   useEffect(() => {
-    fetchHello();
+    fetchPhrases();
   }, []);
 
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
 
-  
   return (
     <>
       <main className="flex h-[calc(100vh-3.5rem)] flex-col items-center justify-between p-24  bg-orange-50">
-      <button className="fixed top-5 right-8 px-2 py-1 rounded hover:bg-red-500 hover:text-orange-50  text-orange-950 ">
+        <button className="fixed top-5 right-8 px-2 py-1 rounded hover:bg-red-500 hover:text-orange-50  text-orange-950 ">
           <Link href="/saved">Saved</Link>
         </button>
-        <section>
-        <h1 className="fixed left-12 w-full text-2xl justify-start">How do I say this <br/><b>professionally</b>?</h1>
-        </section>     
+        <section className=" left-12 flex flex-col w-screen px-10">
+          <h1 className="  text-2xl justify-start pb-6 ">
+            How do I say this <br />
+            <b className="tracking-widest">professionally</b>?
+          </h1>
+          {phrases.map((phrase) => {
+            return (
+              <button className=" inline-block" key={phrase.id}>
+                {phrase.informalPhrase}
+              </button>
+            );
+          })}
+        </section>
       </main>
     </>
   );
