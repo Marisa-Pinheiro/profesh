a16c45361b5bc627623a084fba2c8df17c1c7525
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-09 07:07:58 +0100

    feat: finished first layer of styling in all pages

--- frontend/src/app/phrases/page.tsx
+++ frontend/src/app/phrases/page.tsx
@@ -7,11 +7,11 @@ const Page = () => {
   return (
     <>
       <main className="flex h-[calc(100vh-3.5rem)] flex-col items-center justify-between p-24  bg-orange-50">
-      <button className="fixed top-5 right-8 px-2 py-1 rounded bg-red-500 text-orange-50  hover:bg-orange-950 ">
+      <button className="fixed top-5 right-8 px-2 py-1 rounded hover:bg-red-500 hover:text-orange-50  text-orange-950 ">
           <Link href="/saved">Saved</Link>
         </button>
         <section>
-        <h1>How do I say this <br/><b>professionally</b>?</h1>
+        <h1 className="fixed left-12 w-full text-2xl justify-start">How do I say this <br/><b>professionally</b>?</h1>
         </section>     
       </main>
     </>
