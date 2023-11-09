53e82589f207dfd283fa2364294301cf824b222d
Marisa-Pinheiro <marisa.nbsp@gmail.com>
2023-11-09 07:00:32 +0100

    feat: linked saved page to phrases page

--- frontend/src/app/saved/page.tsx
+++ frontend/src/app/saved/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
     return (
       <>
   <main className="flex h-[calc(100vh-3.5rem)] flex-col items-center justify-between p-24 bg-orange-50">
-        <div><button></button></div>
+      <button className=" hover:bg-red-500 hover:text-orange-50 px-2 py-1 rounded"><Link href="/phrases"> &#8592; Back </Link></button>
         <section>
 
         </section>
